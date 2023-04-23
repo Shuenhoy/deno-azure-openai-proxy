5def58da5fe879f7ee0b5b6e2041e42deeb71692
Haoran SUN <hrsun@zju.edu.cn>
2023-04-23 17:40:53 +0800

    refactor and support embedding

--- README.md
+++ README.md
@@ -1,50 +1,16 @@
-# Deno-Azure-Openai-Proxy
-[![License](https://badgen.net/badge/license/MIT/cyan)](https://github.com/hbsgithub/deno-azure-openai-proxy/blob/main/LICENSE)
-[![Azure](https://badgen.net/badge/icon/Azure?icon=azure&label)](https://github.com/hbsgithub/deno-azure-openai-proxy)
-[![Azure](https://badgen.net/badge/icon/OpenAI?icon=azure&label)](https://github.com/hbsgithub/deno-azure-openai-proxy)
-[![TS](https://badgen.net/badge/icon/typescript?icon=typescript&label)](https://github.com/hbsgithub/deno-azure-openai-proxy)
+# deno-azure-openai-proxy
+Docker image: `shuenhoy/deno-azure-openai-proxy`
+## Changes from the original repo
+- Support embeddings
+- Set configs from environment, e.g.
+  - `AZURE_OPENAI_ENDPOINT=https://test.openai.azure.com/`
+  - `AZURE_OPENAI_API_VER=2023-03-15-preview`
+  - `AZURE_OPENAI_MODEL_MAPPER=gpt-3.5-turbo=test`
+  - `AZURE_OPENAI_TOKEN=xxxx` (optional, when set, the client would not need to send token by http header, use with caution!)
 
- **[English](https://github.com/hbsgithub/deno-azure-openai-proxy/blob/main/README_en.md)**
 
-一个部署Deno Deploy上在 Azure OpenAI API 的代理工具，可以将一个 OpenAI 请求转化为 Azure OpenAI 请求，方便作为各类开源 ChatGPT 的后端使用，无需服务器，免费使用。
-## 特点
-1. 相比于cloudflare workers，可以无需代理，直连使用
-2. 支持自定义二级域名（*.deno.dev）或者绑定自己的域名
-3. 支持打字机模式的流式响应
-4. 支持mapper，可以自定义模型映射规则，也支持直接透传模型名
-5. 无需服务器，免费在线部署，每月10万次请求额度
-# 更新
-- 23.4.12 更新支持mapper模型映射规则
-# 部署方法
-1. 访问 https://dash.deno.com 并注册登录（可以直接使用github账号）
-2. 点击New Project新建项目
-3. 点击playground右侧的play进入编辑模式
-4. 将本项目的 [main.ts](https://github.com/hbsgithub/deno-azure-openai-proxy/blob/main/main.ts)中的内容复制并粘贴到编辑器中
-5. 修改对应的 resourceName 和 mapper
-![部署方法](https://user-images.githubusercontent.com/1295315/229705215-e0556c99-957f-4d98-99a6-1c51254110b9.png)
-   **mapper设置例子**：如果你在azure上部署的gpt-3.5-turbo和gpt-4的模型deployment name为gpt35和gpt4，那么mapper按照如下设置
-
-   ```
-   const mapper:any = {
-     'gpt-3.5-turbo': 'gpt35',
-     'gpt-4': 'gpt4' 
-   };
-   ```
-   其他的map规则直接按这样的格式续写即可
-
-6. 点击save and deploy进行保存并部署
-7. 返回到project的settings中自定义自己的二级域名或者绑定自己的域名
-# 使用方法
-将自己自定义的二级域名或绑定的自定义域名填写到自己所使用的gpt项目的api url中，选择设定过mapper的模型，并在api key 中填写自己的azure openai key即可使用。
 ## License
 MIT
+# Credits
 
-# 备注
-有问题的话欢迎提issue，觉得该项目对你有帮助的话欢迎star！
-
-# 致谢
-Inspired by https://github.com/haibbo/cf-openai-azure-proxy
-
-
-
-
+Forked from https://github.com/hbsgithub/deno-azure-openai-proxy
--- main.ts
+++ main.ts
@@ -1,153 +1,209 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { pooledMap } from "https://deno.land/std@0.182.0/async/pool.ts";
 
-// The name of your Azure OpenAI Resource.
-const resourceName:string = "your-resource-name";
-// The version of OpenAI API.
-const apiVersion:string = "2023-03-15-preview";
-// The mapping of model name.
-const mapper:any = {
-  'gpt-3.5-turbo': 'gpt35',
-  'gpt-4': 'gpt4' 
-  // Other mapping rules can be added here.
+const env = Deno.env;
+
+
+const azureEndpoint = env.get("AZURE_OPENAI_ENDPOINT") ?? throwExpression("please set AZURE_OPENAI_ENDPOINT");
+
+const apiVersion = env.get("AZURE_OPENAI_API_VER") ?? "2023-03-15-preview";
+const mapperString = env.get("AZURE_OPENAI_MODEL_MAPPER") ?? ""
+const token = env.get("AZURE_OPENAI_TOKEN");
+
+const mapper: any = {
+    'gpt-3.5-turbo': 'gpt35',
+    'gpt-4': 'gpt4',
+    ...Object.fromEntries(mapperString.split(",").map(x => x.split("=")))
 };
 
-async function handleRequest(request:Request):Promise<Response> {
-  if (request.method === 'OPTIONS') {
-    return handleOPTIONS(request)
-  }
-
-  const url = new URL(request.url);
-  let path:string;
-  if (url.pathname === '/v1/chat/completions') {
-    path="chat/completions"
-  } else if (url.pathname === '/v1/completions') {
-    path="completions"
-  } else if (url.pathname === '/v1/models') {
-    return handleModels(request)
-  } else {
-    return new Response('404 Not Found', { status: 404 })
-  }
-
-
-  // Get the value of the model field and perform mapping.
-  let deployName:string = '';
-  let body:any;
-  if (request.method === 'POST') {
-    body = await request.json();
-    const modelName:string|undefined = body?.model;
-    if (modelName) {
-      deployName = mapper[modelName] || modelName;
+async function handleRequest(request: Request): Promise<Response> {
+    if (request.method === 'OPTIONS') {
+        return handleOPTIONS(request)
     }
-  }
- 
-  const fetchAPI:string = `https://${resourceName}.openai.azure.com/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`;
 
-  const authKey:string|null = request.headers.get('Authorization');
-  if (!authKey) {
-    return new Response("Not allowed", {status: 403});
-  }
+    const url = new URL(request.url);
 
-  const payload:RequestInit = {
-    method: request.method,
-    headers: {
-      "Content-Type": "application/json",
-      "api-key": authKey.replace('Bearer ', ''),
-    },
-    body: JSON.stringify(body),
-  };
-
-  const { readable, writable } = new TransformStream();
-  const response:Response = await fetch(fetchAPI, payload);
-  if (response.body) {
-    stream(response.body, writable);
-    return new Response(readable, response);
-  } else {
-    throw new Error('Response body is null');
+    if (url.pathname === '/v1/chat/completions') {
+        return handleDirect(request, "chat/completions");
+    } else if (url.pathname === '/v1/completions') {
+        return handleDirect(request, "completions");
+    } else if (url.pathname === '/v1/models') {
+        return handleModels(request)
+    } else if (url.pathname === '/v1/embeddings') {
+        return handleEmbedding(request, "embeddings");
+    } else {
+        return new Response('404 Not Found', { status: 404 })
+    }
 }
 
+async function requestAzure(method: string, body: any, path: string, authKey?: string) {
+    if (!authKey) {
+        return new Response("Not allowed", { status: 403 });
+    }
+
+    // Get the value of the model field and perform mapping.
+    let deployName: string = '';
+    if (method === 'POST') {
+        const modelName: string | undefined = body?.model;
+        if (modelName) {
+            deployName = mapper[modelName] || modelName;
+        }
+    }
+
+    const fetchAPI: string = `${azureEndpoint}/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`;
+
+    const payload: RequestInit = {
+        method: method,
+        headers: {
+            "Content-Type": "application/json",
+            "api-key": authKey,
+        },
+        body: JSON.stringify(body),
+    };
+    return await fetch(fetchAPI, payload);
+}
+
+async function handleDirect(request: Request, path: string) {
+    const [key, body] = await extractRequest(request);
+    const response: Response = await requestAzure(request.method, body, path, key);
+    if (response.body) {
+        const { readable, writable } = new TransformStream();
+        stream(response.body, writable);
+        return new Response(readable, response);
+    } else {
+        throw new Error('Response body is null');
+    }
+}
+
+async function extractRequest(request: Request) {
+    const key = token ?? request.headers.get('Authorization')?.replace('Bearer ', '');
+    const body = request.method === "POST" ? await request.json() : null;
+    return [key, body]
+}
+
+async function handleEmbedding(request: Request, path: string) {
+    const [key, body] = await extractRequest(request);
+    const input = body.input;
+    if (typeof input === "string") {
+        return await requestAzure(request.method, body, path, key);
+    } else if (Array.isArray(input)) {
+        const resps = pooledMap(3,
+            input, x => {
+                return requestAzure(request.method, { ...body, input: x }, path, key);
+            });
+        const retbody = {
+            object: "list",
+            data: [] as any[],
+            model: body.model,
+            usage: {
+                prompt_tokens: 0,
+                total_tokens: 0
+            }
+        };
+        let i = 0;
+        for await (const r of resps) {
+            const ret = await r.json();
+            for (const data of ret.data) {
+                retbody.data.push({ ...data, index: i });
+                i++;
+            }
+            retbody.usage.prompt_tokens += ret.usage.prompt_tokens;
+            retbody.usage.total_tokens += ret.usage.total_tokens;
+        }
+        const json: string = JSON.stringify(retbody, null, 2);
+        return new Response(json, {
+            headers: { 'Content-Type': 'application/json' },
+        });
+    } else {
+        throw new Error('Invalid input type');
+    }
 }
 
 
-function sleep(ms:number):Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // support printer mode and add newline
-async function stream(readable:ReadableStream<Uint8Array>, writable:WritableStream<Uint8Array>):Promise<void> {
-  const reader = readable.getReader();
-  const writer = writable.getWriter();
+async function stream(readable: ReadableStream<Uint8Array>, writable: WritableStream<Uint8Array>): Promise<void> {
+    const reader = readable.getReader();
+    const writer = writable.getWriter();
 
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
 
-  const newline = "\n";
-  const delimiter = "\n\n";
-  const encodedNewline = encoder.encode(newline);
+    const newline = "\n";
+    const delimiter = "\n\n";
+    const encodedNewline = encoder.encode(newline);
 
-  let buffer = "";
-  while (true) {
-    const { value, done } = await reader.read();
-    if (done) {
-      break;
+    let buffer = "";
+    while (true) {
+        const { value, done } = await reader.read();
+        if (done) {
+            break;
+        }
+        buffer += decoder.decode(value, { stream: true }); // stream: true is important here,fix the bug of incomplete line
+        const lines = buffer.split(delimiter);
+
+        // Loop through all but the last line, which may be incomplete.
+        for (let i = 0; i < lines.length - 1; i++) {
+            await writer.write(encoder.encode(lines[i] + delimiter));
+            await sleep(30);
+        }
+
+        buffer = lines[lines.length - 1];
     }
-    buffer += decoder.decode(value, { stream: true }); // stream: true is important here,fix the bug of incomplete line
-    const lines = buffer.split(delimiter);
 
-    // Loop through all but the last line, which may be incomplete.
-    for (let i = 0; i < lines.length - 1; i++) {
-      await writer.write(encoder.encode(lines[i] + delimiter));
-      await sleep(30);
+    if (buffer) {
+        await writer.write(encoder.encode(buffer));
     }
-
-    buffer = lines[lines.length - 1];
-  }
-
-  if (buffer) {
-    await writer.write(encoder.encode(buffer));
-  }
-  await writer.write(encodedNewline)
-  await writer.close();
+    await writer.write(encodedNewline)
+    await writer.close();
 }
 
-async function handleModels(request:Request):Promise<Response> {
-  const data:any = {
-    "object": "list",
-    "data": [ {
-      "id": "gpt-3.5-turbo",
-      "object": "model",
-      "created": 1677610602,
-      "owned_by": "openai",
-      "permission": [{
-        "id": "modelperm-M56FXnG1AsIr3SXq8BYPvXJA",
-        "object": "model_permission",
-        "created": 1679602088,
-        "allow_create_engine": false,
-        "allow_sampling": true,
-        "allow_logprobs": true,
-        "allow_search_indices": false,
-        "allow_view": true,
-        "allow_fine_tuning": false,
-        "organization": "*",
-        "group": null,
-        "is_blocking": false
-      }],
-      "root": "gpt-3.5-turbo",
-      "parent": null
-    }]
-  };
-  const json:string = JSON.stringify(data, null, 2);
-  return new Response(json, {
-    headers: { 'Content-Type': 'application/json' },
-  });
+async function handleModels(request: Request): Promise<Response> {
+    const data: any = {
+        "object": "list",
+        "data": [{
+            "id": "gpt-3.5-turbo",
+            "object": "model",
+            "created": 1677610602,
+            "owned_by": "openai",
+            "permission": [{
+                "id": "modelperm-M56FXnG1AsIr3SXq8BYPvXJA",
+                "object": "model_permission",
+                "created": 1679602088,
+                "allow_create_engine": false,
+                "allow_sampling": true,
+                "allow_logprobs": true,
+                "allow_search_indices": false,
+                "allow_view": true,
+                "allow_fine_tuning": false,
+                "organization": "*",
+                "group": null,
+                "is_blocking": false
+            }],
+            "root": "gpt-3.5-turbo",
+            "parent": null
+        }]
+    };
+    const json: string = JSON.stringify(data, null, 2);
+    return new Response(json, {
+        headers: { 'Content-Type': 'application/json' },
+    });
 }
 
-function handleOPTIONS(request:Request):Response {
+function handleOPTIONS(request: Request): Response {
     return new Response(null, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Headers': '*'
-      }
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*',
+            'Access-Control-Allow-Headers': '*'
+        }
     })
 }
 serve(handleRequest);
+
+function throwExpression(errorMessage: string): never {
+    throw new Error(errorMessage);
+}
